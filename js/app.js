f7ebfada5be45fba835e447464bd7e628ff64855
Dwayne Jones <dwaynejones13123@gmail.com>
2023-01-17 14:10:15 -0500

    project2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('Hello World')
+window.onload = (event) => {
+
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault()
+  
+      const search = document.querySelector('input[type="text"]').value
+  
+      fetch(`https://pokeapi.co/api/v2/pokemon/${search}`).then((response) => {
+        // console.log(response)
+      return response.json()
+      }).then((json) => {
+        console.log(json, 'this was successful')
+        document.querySelector('#name').innerText = json.species.name
+        document.querySelector('#type').innerText = json.types
+        document.querySelector('#ability').innerText = json.abilites
+      },
+      (err) => {
+        console.log(err, 'this was an error')
+      })
+  
+    })
+  
+    
+  
+  }
