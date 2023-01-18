1a4dfb30d4a42054462e2ef7452610fc6c34f594
Dwayne Jones <dwaynejones13123@gmail.com>
2023-01-18 18:16:50 -0500

    project

--- js/app.js
+++ js/app.js
@@ -9,17 +9,15 @@ window.onload = (event) => {
         // console.log(response)
       return response.json()
       }).then((json) => {
-        console.log(json, 'this was successful')
-        document.querySelector('#name').innerText = json.name
-        document.querySelector('#type').innerText = json.types 
-        document.querySelector('#ability').innerText = json.ability
+        console.log(json, 'this was sucessful')
+          
+          doccument.querySelector('#name').innerText = json.Name
+          doccument.querySelector('#types').innerText = json.Types
+          doccument.querySelector('#abilities').innerText = json.abilities
+        
       },
       (err) => {
         console.log(err, 'this was an error')
       })
-  
-    })
-  
-    
-  
-  }
+
+    })}
