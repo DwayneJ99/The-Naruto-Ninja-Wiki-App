ec62c5986b5dcc2de9894e17843791544cad8cbb
Dwayne Jones <dwaynejones13123@gmail.com>
2023-01-18 22:18:37 -0500

    got gif working on html

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ window.onload = (event) => {
           document.querySelector('#types').innerText = json.types[0].type.name
           document.querySelector('#abilities').innerText = json.abilities[0].ability.name
           document.querySelector('#abilities2').innerText = json.abilities[1].ability.name
+        
          
          
         
